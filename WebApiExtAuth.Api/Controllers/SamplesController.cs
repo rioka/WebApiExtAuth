78b09807056127b39c0814366068b095a3820f9f
r.dozzo <r.dozzo@orwellg.com>
2016-01-14 09:19:50 +0100

    Running plain web api sample, no auth yet

---
+++ WebApiExtAuth.Api/Controllers/SamplesController.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Web.Http;
+
+namespace WebApiExtAuth.Api.Controllers
+{
+  public class SamplesController : ApiController
+  {
+    public IHttpActionResult Get()
+    {
+      return Ok(Guid.NewGuid());
+    }
+  }
+}
---
+++ WebApiExtAuth.Api/Startup.cs
@@ -0,0 +1,20 @@
+﻿using System.Web.Http;
+using Microsoft.Owin;
+using Owin;
+using WebApiExtAuth.Api;
+
+[assembly: OwinStartup(typeof(Startup))]
+
+namespace WebApiExtAuth.Api
+{
+  public class Startup
+  {
+    public void Configuration(IAppBuilder app)
+    {
+      var config = new HttpConfiguration();
+      WebApiConfig.Register(config);
+
+      app.UseWebApi(config);
+    }
+  }
+}
